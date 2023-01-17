69aa3330a54f9a2f15b218ccb8514eb455328d99
Alexhaoyu <110823364+Alex-H4O@users.noreply.github.com>
2023-01-17 17:17:09 +1100

    feat: finish forecast function (#15)

--- weather/src/components/Forecast/Forecast.js
+++ weather/src/components/Forecast/Forecast.js
@@ -1,5 +1,8 @@
+import { useState, useEffect } from "react";
+
 import styled from "styled-components";
 import FutureTemp from "./futureTemp/FutureTemp";
+import getForecast from '../apis/getForecast';
 
 const Container = styled.div`
     padding: 36px 48px;
@@ -23,18 +26,44 @@ const TEMPERATURE = [
     {cityId: 4, date:'Fri', weather:{icon:'01n',main:'Rain'}, temp:'8'}
 ]
 
-const Forecast =()=>{
+const Forecast =({
+    id
+})=>{
+    const [forecastData, setForecastData] = useState();
+    useEffect(()=>{
+        const getForecastData = async() =>{
+            const forecastData = await getForecast(id);
+            setForecastData(forecastData);
+        }
+        getForecastData();
+    },[id])
+
+    if(!forecastData){
+        return null
+    }
+
+    const list =  [
+        forecastData.data.list[0],
+        forecastData.data.list[6],
+        forecastData.data.list[14],
+        forecastData.data.list[22],
+        forecastData.data.list[29],
+    ]
+
     return(
         <Container>
             <Title>Forecast</Title>
             <Container2>
-            {TEMPERATURE.map((item)=>{
+            {list && list.map((item)=>{
                 return(
                     <FutureTemp
                         key = {item.cityId}
-                        date = {item.date}
-                        weather = {item.weather}
-                        temp = {item.temp}
+                        date = {item.dt_txt.substring(6,10)}
+                        weather = {{
+                            icon:item.weather[0].icon,
+                            description:item.weather[0].main,
+                        }}
+                        temp = {parseInt(item.main.temp)}
                     />
                 )
             })}
